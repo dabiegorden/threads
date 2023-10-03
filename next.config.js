7072648c8a57f843c57e2a489d20383e9b87c32b
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-10-02 19:53:59 -0700

    comit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoredBuildErrors: true,
+  },
+
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
